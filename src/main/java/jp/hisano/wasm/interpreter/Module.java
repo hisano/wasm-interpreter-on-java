98c2d0cce1b8d7895a3965260b3028cbde8861b1
Koji Hisano <hisano@gmail.com>
2022-04-29 07:07:56 +0900

    Typeセクションの読み取り処理を追加

--- src/main/java/jp/hisano/wasm/interpreter/Module.java
+++ src/main/java/jp/hisano/wasm/interpreter/Module.java
@@ -1,4 +1,26 @@
 package jp.hisano.wasm.interpreter;
 
-public class Module {
+import java.util.ArrayList;
+import java.util.List;
+
+final class Module {
+	private List<FunctionType> functionTypes = new ArrayList<>();
+
+	void addFunctionType(ValueType[] parameterTypes, ValueType[] returnTypes) {
+		functionTypes.add(new FunctionType(parameterTypes, returnTypes));
+	}
+
+	public enum ValueType {
+		I32, I64, F32, F64
+	}
+
+	private class FunctionType {
+		private final ValueType[] parameterTypes;
+		private final ValueType[] returnTypes;
+
+		FunctionType(ValueType[] parameterTypes, ValueType[] returnTypes) {
+			this.parameterTypes = parameterTypes;
+			this.returnTypes = returnTypes;
+		}
+	}
 }
--- src/main/java/jp/hisano/wasm/interpreter/Parser.java
+++ src/main/java/jp/hisano/wasm/interpreter/Parser.java
@@ -1,23 +1,75 @@
 package jp.hisano.wasm.interpreter;
 
 import static jp.hisano.wasm.interpreter.InterpreterException.Type.*;
+import jp.hisano.wasm.interpreter.Module.ValueType;
 
 final class Parser {
 	private static final int MAGIC = 0x6d736100; // = "\0asm"
 	private static final int VERSION = 1;
 
-	private final byte[] _wasmBinary;
+	private final byte[] wasmBinary;
 
-	private int _readIndex;
+	private final Module module = new Module();
+
+	private int readIndex;
 
 	public Parser(byte[] wasmBinary) {
-		_wasmBinary = wasmBinary;
+		this.wasmBinary = wasmBinary;
 	}
 
 	public Module parseModule() {
 		checkInt(MAGIC);
 		checkInt(VERSION);
-		return null;
+
+		while (canRead()) {
+			int section = readByte();
+			int size = readUnsignedLeb128();
+			switch (section) {
+				case 0x01:
+					readTypeSection();
+					break;
+			}
+		}
+
+		return module;
+	}
+
+	private void readTypeSection() {
+		int length = readUnsignedLeb128();
+		int index = 0;
+		while (index < length) {
+			switch (readByte()) {
+				case 0x60:
+					readFunctionType();
+					break;
+			}
+			index++;
+		}
+	}
+
+	private void readFunctionType() {
+		module.addFunctionType(readResultTypes(), readResultTypes());
+	}
+
+	private ValueType[] readResultTypes() {
+		int length = readUnsignedLeb128();
+		ValueType[] result = new ValueType[length];
+
+		int index = 0;
+		while (index < length) {
+			switch (readByte()) {
+				case 0x7f:
+					result[index] = ValueType.I32;
+					break;
+			}
+			index++;
+		}
+
+		return result;
+	}
+
+	private boolean canRead() {
+		return readIndex < wasmBinary.length; 
 	}
 
 	private void checkInt(int expectedValue) {
@@ -27,10 +79,32 @@ final class Parser {
 	}
 
 	private int readInt() {
-		return readByte() | (readByte() << 8) | (readByte() << 16) | (readByte() << 24);
+		return readUnsignedByte() | (readUnsignedByte() << 8) | (readUnsignedLeb128() << 16) | (readUnsignedByte() << 24);
 	}
 
 	private int readByte() {
-		return _wasmBinary[_readIndex++];
+		return wasmBinary[readIndex++];
+	}
+
+	private int readUnsignedByte() {
+		return readByte() & 0xff;
+	}
+
+	private int readUnsignedLeb128() {
+		int result = 0;
+
+		int value;
+		int index = 0;
+		do {
+			value = readUnsignedByte();
+			result |= (value & 0x7f) << (index * 7);
+			index++;
+		} while (((value & 0x80) != 0) && index < 5);
+
+		if ((value & 0x80) != 0) {
+			throw new InterpreterException(ILLEGAL_BINARY);
+		}
+
+		return result;
 	}
 }
