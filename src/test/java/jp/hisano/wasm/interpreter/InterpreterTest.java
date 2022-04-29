81b11ab325af2852f8fbcdcf6238e1a461d093c2
Koji Hisano <hisano@gmail.com>
2022-04-29 13:02:17 +0900

    add関数のテストケースを追加

--- src/test/java/jp/hisano/wasm/interpreter/InterpreterTest.java
+++ src/test/java/jp/hisano/wasm/interpreter/InterpreterTest.java
@@ -3,6 +3,8 @@ package jp.hisano.wasm.interpreter;
 import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static com.google.common.io.Resources.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,11 +12,34 @@ import static org.junit.jupiter.api.Assertions.*;
 class InterpreterTest {
 	@Test
 	void addTwo() throws IOException {
-		byte[] wasm = toByteArray(getResource(InterpreterTest.class, "basic/addTwo.wasm"));
-		Interpreter interpreter = new Interpreter(wasm);
+		Interpreter interpreter = createInterpreter("basic/addTwo.wasm");
 
-		int result = (Integer) interpreter.getExportedFunction("addTwo").invoke(1, 1);
+		int resultValue = (Integer) interpreter.getExportedFunction("addTwo").invoke(1, 1);
 
-		assertEquals(2, result);
+		assertEquals(2, resultValue);
+	}
+
+	private Interpreter createInterpreter(String wasmFilePath) throws IOException {
+		byte[] wasmBinary = toByteArray(getResource(InterpreterTest.class, wasmFilePath));
+		return new Interpreter(wasmBinary);
+	}
+
+	@ParameterizedTest(name = "{0} + {1} = {2} (i32)")
+	@CsvSource({
+		"1,1,2",
+		"1,0,1",
+		"4294967295,4294967295,4294967294",
+		"4294967295,1,0",
+		"2147483647,1,2147483648",
+		"2147483648,4294967295,2147483647",
+		"2147483648,2147483648,0",
+		"1073741823,1,1073741824",
+	})
+	void add(long first, long second, long expectedValue) throws IOException {
+		Interpreter interpreter = createInterpreter("spec/i32.0.wasm");
+
+		int resultValue = (Integer) interpreter.getExportedFunction("add").invoke((int)first, (int)second);
+
+		assertEquals((int)expectedValue, resultValue);
 	}
 }
