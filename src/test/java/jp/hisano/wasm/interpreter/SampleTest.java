686e7df74e7a215e699a4053877c5f42f9ac7562
Koji Hisano <hisano@gmail.com>
2022-05-01 12:21:51 +0900

    サンプルのテストケースを改良

--- src/test/java/jp/hisano/wasm/interpreter/SampleTest.java
+++ src/test/java/jp/hisano/wasm/interpreter/SampleTest.java
@@ -7,10 +7,10 @@ import org.junit.jupiter.api.Test;
 import static jp.hisano.wasm.interpreter.TestUtils.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-class InterpreterTest {
+class SampleTest {
 	@Test
 	void addTwo() throws IOException {
-		int resultValue = createInterpreter("basic/addTwo.wasm").invoke("addTwo", 1, 1);
+		int resultValue = createInterpreter("sample/add.wasm").invoke("add", 1, 1);
 		assertEquals(2, resultValue);
 	}
 }
