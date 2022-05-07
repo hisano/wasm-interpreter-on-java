8ac9a951ef422472a6bb822bbcb05305e5d2a862
Koji Hisano <hisano@gmail.com>
2022-05-08 03:41:13 +0900

    型チェック機能を追加

--- src/main/java/jp/hisano/wasm/interpreter/Value.java
+++ src/main/java/jp/hisano/wasm/interpreter/Value.java
@@ -17,22 +17,22 @@ final class Value {
 
 	Value(int i32Value) {
 		this(I32);
-		this.i32Value = i32Value; 
+		setI32(i32Value);
 	}
 
 	Value(long i64Value) {
 		this(I64);
-		this.i64Value = i64Value;
+		setI64(i64Value);
 	}
 
 	Value(float f32Value) {
 		this(F32);
-		this.f32Value = f32Value;
+		setF32(f32Value);
 	}
 
 	Value(double f64Value) {
 		this(F64);
-		this.f64Value = f64Value;
+		setF64(f64Value);
 	}
 
 	ValueType getType() {
@@ -40,34 +40,48 @@ final class Value {
 	}
 
 	void setI32(int newValue) {
+		assureType(I32);
 		i32Value = newValue;
 	}
 
 	int getI32() {
+		assureType(I32);
 		return i32Value;
 	}
 
 	void setI64(long newValue) {
+		assureType(I64);
 		i64Value = newValue;
 	}
 
 	long getI64() {
+		assureType(I64);
 		return i64Value;
 	}
 
 	void setF32(float newValue) {
+		assureType(F32);
 		f32Value = newValue;
 	}
 
 	float getF32() {
+		assureType(F32);
 		return f32Value;
 	}
 
 	void setF64(double newValue) {
+		assureType(F64);
 		f64Value = newValue;
 	}
 
 	double getF64() {
+		assureType(F64);
 		return f64Value;
+	}
+
+	private void assureType(ValueType type) {
+		if (this.type != type) {
+			throw new TrapException("type mismatch");
+		}
 	}
 }
